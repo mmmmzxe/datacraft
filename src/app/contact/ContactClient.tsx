673872f088ccc6497a695514571f3226f6d08aeb
Maryem Mostafa <maryem.mostafa@yourparts.com>
2026-03-02 00:50:13 +0200

    fix: Remove unnecessary empty div in ContactClient component

--- src/app/contact/ContactClient.tsx
+++ src/app/contact/ContactClient.tsx
@@ -161,7 +161,6 @@ export default function ContactClient() {
                   </a>
                  
                 </div>
-                
               </div>
             </div>
           </div>
